fc69bd2cf49f9bcc24cecd88fa78802f018d0a0a
subash2617 <74961842+subash2617@users.noreply.github.com>
2021-08-10 20:00:02 +0530

    Update LanguageInfo.java

--- Class-Method-Object/LanguageDetails/src/org/lang/LanguageInfo.java
+++ Class-Method-Object/LanguageDetails/src/org/lang/LanguageInfo.java
@@ -1,7 +1,10 @@
+// Package
 package org.lang;
 
+// Class
 public class LanguageInfo {
 
+	// Method
 	public void tamilLanguage() {
 		System.out.println("Tamil Language");
 	}
@@ -16,9 +19,12 @@ public class LanguageInfo {
 	
 	public static void main(String[] args) {
 		
+		// Object From the Same class
 		LanguageInfo lan = new LanguageInfo();
+		// Object From the different class
 		StateDetails st = new StateDetails();
 		
+		// calling the Method
 		lan.tamilLanguage();
 		lan.englishLanguage();
 		lan.hindiLanguage();
