db9222955304ff71c793d3e0f552e598d439799f
subash2617 <74961842+subash2617@users.noreply.github.com>
2021-08-10 20:02:46 +0530

    Update StateDetails.java

--- Class-Method-Object/LanguageDetails/src/org/lang/StateDetails.java
+++ Class-Method-Object/LanguageDetails/src/org/lang/StateDetails.java
@@ -1,7 +1,10 @@
+// Package
 package org.lang;
 
+// Another Class from the same Package
 public class StateDetails {
 
+	// Method
 	public void southIndia() {
 		System.out.println("South India");
 	}
