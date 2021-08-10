c8ae0febc814233ec42f38c217a5bbe6e8ed7db3
subash2617 <74961842+subash2617@users.noreply.github.com>
2021-08-10 20:10:15 +0530

    Update Employee.java

--- Class-Method-Object/EmployeeInformation/src/org/emp/Employee.java
+++ Class-Method-Object/EmployeeInformation/src/org/emp/Employee.java
@@ -1,22 +1,29 @@
+// Main Package
 package org.emp;
 
+// Importing all the Packages
 import org.client.Client;
 import org.company.Company;
 import org.project.Project;
 
+// Class
 public class Employee {
 
+	// Method
 	public void empName() {
 		System.out.println("Subash");
 	}
 	
+	// Main
 	public static void main(String[] args) {
 		
+		// Object
 		Employee emp = new Employee();
 		Company com = new Company();
 		Client cl = new Client();
 		Project p = new Project();
 		
+		// Calling the Methods from all the Packages & Classes
 		emp.empName();
 		com.companyName();
 		cl.clientName();
